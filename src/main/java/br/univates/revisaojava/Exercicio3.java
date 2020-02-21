cea3e9613cb80b76caaa9e7f9c63d87f9acec206
jdhein <jdhein@192.168.56.1>
2020-02-20 19:05:57 -0600

    Exercício 3

--- src/main/java/br/univates/revisaojava/Exercicio3.java
+++ src/main/java/br/univates/revisaojava/Exercicio3.java
@@ -22,6 +22,7 @@ public class Exercicio3 {
         objConta1.conta = 444;
         objConta1.nome = "Juca Bala";
         objConta1.saldo = 200.99;
+        objConta1.cheque_especial = 500;//Quanto ela pode ficar negativa
         objConta1.imprimir();
         
         Conta objConta2 = new Conta();    
@@ -30,6 +31,7 @@ public class Exercicio3 {
         objConta2.conta = 658;
         objConta2.nome = "Pedro Rocha";
         objConta2.saldo = 980.65;
+        objConta2.cheque_especial = 500;//Quanto ela pode ficar negativa
         objConta2.imprimir();
         
         /*Transferencia de valor entre contas
@@ -37,14 +39,13 @@ public class Exercicio3 {
           Qual o valor?
           A conta de origem fica sendo a conta do objeto em si
         */
-        double valorTransf = 500.80;
+        double valorTransf = 1500.80;
         objConta1.transferirValor(objConta2, valorTransf);
         
         System.out.println("\nDEPOIS");
         objConta1.imprimir();
         objConta2.imprimir();
-        
-    
+       
     }
     
 }
--- src/main/java/dao/Conta.java
+++ src/main/java/dao/Conta.java
@@ -10,6 +10,7 @@ public class Conta {
     public int conta;
     public String nome;
     public double saldo;
+    public double cheque_especial;//valor de limite a ser utilizado caso o saldo fique negativo
     
     public void imprimir(){
         System.out.println("Conta: " + conta + "\nSaldo: " + saldo + "\n----------");
@@ -17,9 +18,16 @@ public class Conta {
     
     //Transfere o valor de uma conta para outra
     public void transferirValor(Conta contaDestino, double valorTransferencia){
-        //passar valorTransferencia da conta origem para a conta destino
-        this.saldo = saldo - valorTransferencia; //tira o saldo da conta origem
-        contaDestino.saldo += valorTransferencia; //incrementar o saldo da conta de destino
+        //Além de testar o saldo,
+        //precisa testar o cheque especial
+        if(this.saldo >= valorTransferencia){
+            //passar valorTransferencia da conta origem para a conta destino
+            this.saldo = saldo - valorTransferencia; //tira o saldo da conta origem
+            contaDestino.saldo += valorTransferencia; //incrementar o saldo da conta de destino
+        }else{
+            System.out.println("Ops, saldo insuficiente");
+        }
+        
     }
     
 }
